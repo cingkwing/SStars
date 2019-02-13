6d14f71c2a91bede2feb321098ad208e351f08a9
cingkwing <53495404@qq.com>
2019-02-13 11:43:58 +0800

    提交了一个简单的、难看的新事项编辑活动xml及无内容的对应java文件

---
+++ SStars/app/src/main/java/com/example/lihengdong/administrator/sstars/Activity_login.java
@@ -0,0 +1,15 @@
+package com.example.administrator.sstars;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lihengdong.sstars.R;
+
+public class Activity_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
